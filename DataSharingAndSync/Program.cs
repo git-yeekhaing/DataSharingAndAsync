0a04bde90331bea42e27e9651c1aaaf5dc6b7d78
myanmmar-polestar <mm.coursefinder@gmail.com>
2020-06-06 20:11:56 +0900

    with locking

---
+++ DataSharingAndSync/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSharingAndSync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CriticalSession();
+            Console.ReadLine();
+        }
+
+        public static void CriticalSession()
+        {
+            var tasks = new List<Task>();
+            BankAccount ba = new BankAccount();
+
+            tasks.Add(Task.Factory.StartNew(() =>
+            {
+                for (int j = 0; j < 1000; j++)
+                {
+                    ba.Deposit(100);
+                }
+            }));
+
+            tasks.Add(Task.Factory.StartNew(() =>
+            {
+                for (int j = 0; j < 1000; j++)
+                {
+                    ba.Withdraw(100);
+                }
+            }));
+
+            Task.WaitAll(tasks.ToArray());
+            Console.WriteLine($"Final balance is {ba.Balance}.");
+            Console.WriteLine("All done here.");
+        }
+    }
+
+    class BankAccount
+    {
+        public int Balance { get; set; }
+
+        public void Deposit(int amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(int amount)
+        {
+            Balance -= amount;
+        }
+    }
+}
